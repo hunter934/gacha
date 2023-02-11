fca19706c47c23b5b75d34088c1cf10fb7876aa1
Farizal Hanafi <102751082+hunter934@users.noreply.github.com>
2023-02-11 14:16:55 +0700

    Update gacha.py

--- gacha.py
+++ gacha.py
@@ -18,19 +18,22 @@ def gacha(mylist):
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    button2000 = st.button("IDR 2000")
+    button2000 = st.button("ROLL")
+    st.write("IDR 2000")
     st.write(button2000)
     if button2000:
         gacha(mylist200)
 
 with col2:
-    button5000 = st.button("IDR 5000")
+    button5000 = st.button("ROLL")
+    st.write("IDR 5000")
     st.write(button5000)
     if button5000:
         gacha(mylist100)
 
 with col3:
-    button10000 = st.button("IDR 10000")
+    button10000 = st.button("ROLL")
+    st.write("IDR 10000")
     st.write(button10000)
     if button10000:
         gacha(mylist50)

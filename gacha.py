3b37a767f868fb795f66fb2bc255520c3dd69574
Farizal Hanafi <102751082+hunter934@users.noreply.github.com>
2023-02-11 11:38:16 +0700

    Update gacha.py

--- gacha.py
+++ gacha.py
@@ -13,7 +13,8 @@ mylist50 = range(50)
 def gacha(mylist):
     nums = random.choices(mylist, weights=None)
     strnums = str(nums)
-    st.button(strnums)
+    result = st.header(strnums)
+    st.button(result)
 
 col1, col2, col3 = st.columns(3)
 
